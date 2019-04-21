f9ca1853ce85ad66c5a6f9575e4f8906d37017e8
Davide Daffonchio <davided95@hotmail.it>
2019-04-21 15:57:05 +0200

    Add files via upload

---
+++ Lab4Prof/Replace.implementation/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("Replace.implementation")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Replace.implementation")]
+[assembly: AssemblyCopyright("Copyright ©  2018")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("588a6391-d203-4d7b-ba20-4b2892272091")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ Lab4Prof/Replace.implementation/Replace.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Replace.Implementation
+{
+    public class Replace
+    {
+        public static IEnumerable<T> MacroExpansion<T>(IEnumerable<T> sequence, T values, IEnumerable<T> newValues)
+        {
+            CheckNotNull(sequence, nameof(sequence));
+            CheckNotNull(newValues,nameof(newValues));
+            foreach (var elem in sequence)
+            {
+                if (Equals(elem, values))
+                    foreach (var e in newValues)
+                    {
+                        yield return e;
+                    }
+                else yield return elem;
+            }
+        }
+        
+
+        private static void CheckNotNull<T>(IEnumerable<T> parameter, string parameterName)
+        {
+            if (null == parameter)
+            {
+            throw new ArgumentNullException(nameof(parameter), " sequence cannot be null");
+            }
+        }
+    }
+}
