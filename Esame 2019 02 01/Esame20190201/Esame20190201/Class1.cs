9353158a72ec46ed54cd583e53da3d5509bcac45
Davide Daffonchio <davided95@hotmail.it>
2019-04-23 00:03:47 +0200

    updated 20190201

--- Esame 2019 02 01/Esame20190201/Esame20190201/Class1.cs
+++ Esame 2019 02 01/Esame20190201/Esame20190201/Class1.cs
@@ -3,12 +3,10 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using NUnit.Framework;
 
 namespace Esame20190201
 {
-    public class Class1
-    {
-    }
 
     public static class MyExtensions
     {
@@ -17,7 +15,47 @@ namespace Esame20190201
             if (s == null)
                 throw new ArgumentNullException();
 
-            var en = s.GetEnumerator();
+            var now = s.GetEnumerator();
+            now.Reset();
+            var future = s.GetEnumerator();
+            if (!future.MoveNext())
+                return s;
+            var resultList = new List<T>();
+
+            do
+            {
+                //swap
+                resultList.Add(future.Current);
+                resultList.Add(now.Current);
+
+                //jump 2
+                now.MoveNext();
+                if (!future.MoveNext())
+                    return resultList;
+                now.MoveNext();
+            } while (future.MoveNext());
+
+            return resultList;
+
+        }
+    }
+
+    [TestFixture]
+    public class MyTest
+    {
+        [Test]
+        public void Null_sequence_throws()
+        {
+            List<int> s = null;
+            Assert.That(() => s.EvenOddSwap(), Throws.TypeOf<ArgumentNullException>());
+        }
+
+        [Test]
+        public void List_swap_returns()
+        {
+            var s = new List<string>() { "Donald Duck", "Louie", "Dewey", "Huey", "Scrooge McDuck" };
+            var expected = new List<string>() { "Louie", "Donald Duck", "Huey", "Dewey", "Scrooge McDuck" };
+            Assert.That(s.EvenOddSwap(), Is.EqualTo(expected));
         }
     }
 }
