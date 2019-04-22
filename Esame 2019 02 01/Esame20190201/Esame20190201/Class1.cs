6581151cc8c98c0ab947e0a635dbd26d4883fae1
Davide Daffonchio <davided95@hotmail.it>
2019-04-22 21:26:46 +0200

    first commit

---
+++ Esame 2019 02 01/Esame20190201/Esame20190201/Class1.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Esame20190201
+{
+    public class Class1
+    {
+    }
+
+    public static class MyExtensions
+    {
+        public static IEnumerable<T> EvenOddSwap <T>(this IEnumerable<T> s)
+        {
+            if (s == null)
+                throw new ArgumentNullException();
+
+            var en = s.GetEnumerator();
+        }
+    }
+}
